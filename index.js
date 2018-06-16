5330da59a2eaf5edd3688aa89fdc20600466ba8e
Emmanuel Ogbizi <virgoe9@yahoo.com>
2018-06-16 13:22:55 -0400

    feat: respond randomly when user posts after hours (#2)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 /**
  * Define a function for initiating a conversation on installation
- * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
+ * With custom integrations, we don"t have a way to find out who installed us, so we can"t message them :(
  */
 
 function onInstallation(bot, installer) {
@@ -16,8 +16,8 @@ function onInstallation(bot, installer) {
             if (err) {
                 console.log(err);
             } else {
-                convo.say('I am a bot that has just joined your team');
-                convo.say('You must now /invite me to a channel so that I can be of use!');
+                convo.say("I am a bot that has just joined your team");
+                convo.say("You must now /invite me to a channel so that I can be of use!");
             }
         });
     }
@@ -30,7 +30,7 @@ function onInstallation(bot, installer) {
 
 var config = {};
 if (process.env.MONGOLAB_URI) {
-    var BotkitStorage = require('botkit-storage-mongo');
+    var BotkitStorage = require("botkit-storage-mongo");
     config = {
         storage: BotkitStorage({
             mongoUri: process.env.MONGOLAB_URI
@@ -38,7 +38,7 @@ if (process.env.MONGOLAB_URI) {
     };
 } else {
     config = {
-        json_file_store: ((process.env.TOKEN) ? './db_slack_bot_ci/' : './db_slack_bot_a/'), //use a different name if an app or CI
+        json_file_store: ((process.env.TOKEN) ? "./db_slack_bot_ci/" : "./db_slack_bot_a/"), //use a different name if an app or CI
     };
 }
 
@@ -48,15 +48,15 @@ if (process.env.MONGOLAB_URI) {
 
 if (process.env.TOKEN || process.env.SLACK_TOKEN) {
     //Treat this as a custom integration
-    var customIntegration = require('./lib/custom_integrations');
+    var customIntegration = require("./lib/custom_integrations");
     var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
     var controller = customIntegration.configure(token, config, onInstallation);
 } else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
     //Treat this as an app
-    var app = require('./lib/apps');
+    var app = require("./lib/apps");
     var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
 } else {
-    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
+    console.log("Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment");
     process.exit(1);
 }
 
@@ -64,18 +64,18 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
 /**
  * A demonstration for how to handle websocket events. In this case, just log when we have and have not
  * been disconnected from the websocket. In the future, it would be super awesome to be able to specify
- * a reconnect policy, and do reconnections automatically. In the meantime, we aren't going to attempt reconnects,
+ * a reconnect policy, and do reconnections automatically. In the meantime, we aren"t going to attempt reconnects,
  * WHICH IS A B0RKED WAY TO HANDLE BEING DISCONNECTED. So we need to fix this.
  *
  * TODO: fixed b0rked reconnect behavior
  */
 // Handle events related to the websocket connection to Slack
-controller.on('rtm_open', function (bot) {
-    console.log('** The RTM api just connected!');
+controller.on("rtm_open", function (bot) {
+    console.log("** The RTM api just connected!");
 });
 
-controller.on('rtm_close', function (bot) {
-    console.log('** The RTM api just closed');
+controller.on("rtm_close", function (bot) {
+    console.log("** The RTM api just closed");
     // you may want to attempt to re-open
 });
 
@@ -84,6 +84,8 @@ controller.on('rtm_close', function (bot) {
  * Core bot logic goes here!
  */
 // BEGIN EDITING HERE!
+var moment = require("moment");
+
 var Bot = function (controller) {
     var spiel = {
         no: "I'm sorry. I'm afraid I can't do that",
@@ -101,7 +103,7 @@ var Bot = function (controller) {
             name: message.text.split(" ")[1]
         };
         var onError = function (e, response) {
-            console.log(response);
+            console.log("joinleave:", response);
             self.reply(message, spiel.no);
         };
 
@@ -111,7 +113,7 @@ var Bot = function (controller) {
     };
 
     var handleDM = function (self, message) {
-        console.log(message);
+        console.log("direct:", message);
         var action = message.match[0];
         switch (action) {
             case vocab.control.leave:
@@ -123,16 +125,37 @@ var Bot = function (controller) {
         }
     };
 
+    var isLate = function (timestamp) {
+        var dayStartToday = moment().hour(7).minute(0);
+        var dayEndToday = dayStartToday.clone().add(12, "h");
+        console.log("time:", timestamp, dayStartToday.toISOString(), dayEndToday.toDate());
+        return !moment(timestamp * 1000).isBetween(
+            dayStartToday.toISOString(), dayEndToday.toISOString()
+        );
+    };
+
+    var isTired = function () {
+        return Math.random() < 0.7;
+    };
+
+    var generateGoHome = function (message) {
+        var options = [
+            "Go home!", "Are you homeless?",
+            "Stop working!", "Why are you here?"
+        ];
+        return options[Math.floor(Math.random() * options.length)];
+    };
+
     return {
         registerListeners: function () {
-            controller.on('bot_channel_join', this.handleNewRoom);
-            controller.on('bot_group_join', this.handleNewRoom);
+            controller.on("bot_channel_join", this.handleNewRoom);
+            controller.on("bot_group_join", this.handleNewRoom);
             controller.hears(
                 Object.values(vocab.control),
-                'direct_message',
+                "direct_message",
                 this.handleDM
             );
-            controller.on('ambient', this.handleChatter);
+            controller.on("ambient", this.handleChatter);
         },
         handleNewRoom: function (self, message) {
             console.log(message);
@@ -141,6 +164,9 @@ var Bot = function (controller) {
         handleDM: handleDM,
         handleChatter: function (self, message) {
             console.log("go home!", message);
+            if (isLate(Number(message.ts)) && !isTired()) {
+                self.reply(message, generateGoHome(message));
+            }
         }
     };
 };
